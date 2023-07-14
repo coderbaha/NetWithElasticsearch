ab23e152c862096f863658c156f52eaddd510bda
bahaask <bahaask41@gmail.com>
2023-07-14 23:28:32 +0300

    first commit

---
+++ NetWithElasticsearch/Context/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NetWithElasticsearch.Context
+{
+    public sealed class AppDbContext : DbContext
+    {
+        private readonly string _databaseConnection;
+        public AppDbContext(string databaseConnection)
+        {
+            _databaseConnection = databaseConnection;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_databaseConnection);
+        }
+
+        public DbSet<Travel> Travels { get; set; }
+    }
+
+    public sealed class Travel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
